e52dc1c2b854e2ceb7d7bd02e52ab76e3aafc836
moncef <moncxff@gmail.com>
2024-01-31 00:53:21 +0100

    build fix

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Framework7 from 'framework7/lite-bundle';
+import Framework7 from 'framework7/lite/bundle';
 import Framework7React from 'framework7-react';
 
 Framework7.use(Framework7React);
